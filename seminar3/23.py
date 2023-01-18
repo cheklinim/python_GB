9c5d03f31a46c9d854fb198ef7ce8a1c506356c9
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2023-01-18 11:52:01 +0300

    task23 refactor

--- seminar3/23.py
+++ seminar3/23.py
@@ -15,3 +15,12 @@ for i in range(len(data) - 1):
     if data[i + 1] > data[i]:
         count += 1
 print(count)
+
+"""
+list_1 = [0, -1, 5, 2, 3]
+count = 0
+for i in range(1, len(list_1)):
+    if list_1[i] > list_1[i - 1]:
+        count += 1
+print(count)
+"""
