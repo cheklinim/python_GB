0e742f2a3915598a0ccee833749c79b86a572956
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2023-01-18 14:49:01 +0300

    task27 added

---
+++ seminar4/27.py
@@ -0,0 +1,21 @@
+"""
+Пользователь вводит текст(строка).
+Словом считается последовательность непробельных символов идущих подряд,
+слова разделены одним или большим числом пробелов.
+Определите, сколько различных слов содержится в этом тексте.
+
+Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
+
+Output: 13
+"""
+
+line = input()
+print(len(set(line.split())))
+
+"""
+text = input().split()
+set_result = set()
+for i in text:
+    set_result.add(i.lower())
+print(len(set_result))
+"""
